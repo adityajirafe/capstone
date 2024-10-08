ff9afeed190e3aa7a6177b0b0ac0e0ab7e47de6e
ca-leb <wolbelac@gmail.com>
2024-10-08 13:17:25 +0800

    initial auth commit

--- esg-dashboard/src/App.tsx
+++ esg-dashboard/src/App.tsx
@@ -1,13 +1,54 @@
-import './App.css'
-import AppRoutes from './routes'
+import "./App.css";
+import AppRoutes from "./routes";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-function App() {
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+export default function App() {
+  const [session, setSession] = useState(null);
 
-  return (
-    <div className='root'>
-      <AppRoutes />
-    </div>
-  )
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+  if (!session) {
+    return (
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["google"]}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <AppRoutes />
+        <button onClick={() => supabase.auth.signOut()}>Sign out</button>
+      </div>
+    );
+  }
 }
-
-export default App
