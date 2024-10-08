de48b641dcde6bac4e7d0f3304432259798831f2
ca-leb <wolbelac@gmail.com>
2024-10-08 13:17:25 +0800

    initial auth commit

--- esg-dashboard/src/pages/Auth/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-
-const LoginPage = () => (
-  <>
-    <h1>hello world</h1>
-  </>
-);
-
-export default LoginPage;
--- esg-dashboard/src/routes.tsx
+++ esg-dashboard/src/routes.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/Auth/LoginPage'
-import DataInputForm from './pages/DataForm/DataForm';
-import TableauDashboard from './pages/Dashboard/Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DataInputForm from "./pages/DataForm/DataForm";
+import TableauDashboard from "./pages/Dashboard/Dashboard";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LoginPage />} />
       <Route path="/data-input" element={<DataInputForm />} />
-      <Route path="/dashboard" element={<TableauDashboard />} />
+      <Route path="/" element={<TableauDashboard />} />
     </Routes>
   </BrowserRouter>
 );
