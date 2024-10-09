b76338a7e34ba2c933290682cb177db8ba9ca6b8
adityajirafe <adityavj2000@yahoo.com.sg>
2024-10-10 00:41:11 +0800

    ui improvements

--- esg-dashboard/src/components/Header/Header.tsx
+++ esg-dashboard/src/components/Header/Header.tsx
@@ -3,19 +3,21 @@ import { Box, Text, useColorMode } from "@chakra-ui/react";
 import HeaderDark from '../../assets/HeaderDark.svg?react';
 import HeaderLight from '../../assets/HeaderLight.svg?react';
 import { useSupabase } from '../../hooks/useSupabase';
+import ToggleDarkMode from '../ToggleDarkMode';
 
 const Header = () => {
-  const {colorMode} = useColorMode();
+  const { colorMode } = useColorMode();
   const { supabase } = useSupabase();
 
   return (
-    <Box as="header" display="flex" alignItems="center" justifyContent="space-between" className="main" bg="accent.500">
+    <Box as="header" display="flex" alignItems="center" justifyContent="space-between" className="main" bg="background">
       {colorMode == "dark" ? <HeaderDark width="240" height="100" className="header-logo"/> : <HeaderLight width="240" height="100" className="header-logo"/> }
       <Box className='menu-container'>
         <Text onClick={() => {}} className='menu-item' fontSize="md">About Us</Text>
         <Text onClick={() => {}} className='menu-item' fontSize="md">Report</Text>
         <Text onClick={() => {}} className='menu-item' fontSize="md">Dashboard</Text>
         <Text onClick={() => supabase.auth.signOut()} className='menu-item' fontSize="md">Sign Out</Text>
+        <ToggleDarkMode />
       </Box>
     </Box>
   )
