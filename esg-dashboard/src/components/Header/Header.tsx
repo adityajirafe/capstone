0048e79956419c534d25b36a0cfe222ddae79a28
ca-leb <wolbelac@gmail.com>
2024-10-16 17:02:19 +0800

    feat: routing for scraper page

--- esg-dashboard/src/components/Header/Header.tsx
+++ esg-dashboard/src/components/Header/Header.tsx
@@ -1,37 +1,51 @@
-import './Header.css'
+import "./Header.css";
 import { Box, Text, useColorMode } from "@chakra-ui/react";
-import HeaderDark from '../../assets/HeaderDark.svg?react';
-import HeaderLight from '../../assets/HeaderLight.svg?react';
-import { useSupabase } from '../../hooks/useSupabase';
-import ToggleDarkMode from '../ToggleDarkMode';
-import { NavLink, useNavigate } from 'react-router-dom';
-import Paths from '../../router/paths';
+import HeaderDark from "../../assets/HeaderDark.svg?react";
+import HeaderLight from "../../assets/HeaderLight.svg?react";
+import { useSupabase } from "../../hooks/useSupabase";
+import ToggleDarkMode from "../ToggleDarkMode";
+import { NavLink, useNavigate } from "react-router-dom";
+import Paths from "../../router/paths";
 
 const Header = () => {
   const { colorMode } = useColorMode();
   const { supabase, session } = useSupabase();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSignout = () => {
     supabase.auth.signOut();
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   return (
-    <Box as="header" display="flex" alignItems="center" justifyContent="space-between" className="main" bg="background">
-      {colorMode == "dark" ? <HeaderDark width="240" height="100" className="header-logo"/> : <HeaderLight width="240" height="100" className="header-logo"/> }
-      <Box className='menu-container'>
+    <Box
+      as="header"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      className="main"
+      bg="background"
+    >
+      {colorMode == "dark" ? (
+        <HeaderDark width="240" height="100" className="header-logo" />
+      ) : (
+        <HeaderLight width="240" height="100" className="header-logo" />
+      )}
+      <Box className="menu-container">
         <NavLink to={Paths.home}>About Us </NavLink>
-        <NavLink to={Paths.home}>Report</NavLink>
+        <NavLink to={Paths.scraper}>Report</NavLink>
         <NavLink to={Paths.dashboard}>Dashboard</NavLink>
-        {session ? 
-          <Text onClick={handleSignout} className='menu-item'>Sign Out</Text> :
+        {session ? (
+          <Text onClick={handleSignout} className="menu-item">
+            Sign Out
+          </Text>
+        ) : (
           <NavLink to={Paths.login}>Sign In</NavLink>
-        }
+        )}
         <ToggleDarkMode />
       </Box>
     </Box>
-  )
+  );
 };
 
 export default Header;
